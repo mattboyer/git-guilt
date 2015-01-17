4e97596787e879305765c683916109bdf521e74e
Matt Boyer <mboyer@sdf.org>
2015-01-17 21:23:13 +0000

    Fixed PEP8 violation

--- src/guilt.py
+++ src/guilt.py
@@ -47,7 +47,7 @@ class GitRunner(object):
         if self._git_toplevel:
             popen_kwargs['cwd'] = self._git_toplevel
 
-        git_process = subprocess.Popen( #pylint: disable=W0142
+        git_process = subprocess.Popen(  # pylint: disable=W0142
             [GitRunner._git_executable] + args,
             **popen_kwargs
         )
