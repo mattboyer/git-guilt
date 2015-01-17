1b45a4fe68c036d8e89a505299e44f5fecf97105
Matt Boyer <mboyer@sdf.org>
2015-01-17 21:55:12 +0000

    Fixed mocking/patching

--- test/test_guilt.py
+++ test/test_guilt.py
@@ -1,3 +1,4 @@
+import io
 from mock import patch, Mock, call
 from unittest import TestCase
 import test.constants
@@ -25,11 +26,14 @@ class ArgTestCase(TestCase):
         self._popen_patch.stop()
 
     @patch('sys.argv', ['arg0', 'foo'])
-    def test_bad_args(self):
+    @patch('sys.stderr', new_callable=io.StringIO)
+    def test_bad_args(self, mock_stderr):
+
         with self.assertRaises(guilt.GitError):
             self.guilt.process_args()
 
         self.assertEquals(1, self.guilt.run())
+        self.assertEquals('bad args', mock_stderr.getvalue())
 
     @patch('sys.argv', ['arg0', '--help'])
     def test_help(self):
@@ -43,63 +47,70 @@ class ArgTestCase(TestCase):
 class GitRunnerTestCase(TestCase):
 
     def setUp(self):
+        # We need to mock up subprocess.Popen so that the 'git' invocation
+        # performed when the GitRunner object is instantiated doesn't result in
+        # an actual process being forked
+        self._popen_patch = patch('guilt.subprocess.Popen')
+        self.mocked_popen = self._popen_patch.start()
+        self.mocked_popen.return_value = Mock(
+            communicate=Mock(return_value=('bar', None))
+        )
+
+        self.runner = guilt.GitRunner()
+        self._popen_patch.stop()
+
+    def tearDown(self):
         pass
 
     @patch('guilt.subprocess.Popen')
     def test_run_git_cwd(self, mock_process):
         mock_process.return_value.communicate = Mock(return_value=('bar', None))
 
-        runner = guilt.GitRunner()
-        runner._git_toplevel = None
-        runner._run_git(['foo'])
+        self.runner._git_toplevel = None
+        self.runner._run_git(['foo'])
 
         mock_process.assert_called_once_with(['nosuchgit', 'foo'], stderr=-1, stdout=-1, universal_newlines=True)
         mock_process.reset_mock()
 
-        runner._git_toplevel = '/my/top/level/git/directory'
-        runner._run_git(['foo'])
+        self.runner._git_toplevel = '/my/top/level/git/directory'
+        self.runner._run_git(['foo'])
         mock_process.assert_called_once_with(['nosuchgit', 'foo'], cwd='/my/top/level/git/directory', stderr=-1, stdout=-1, universal_newlines=True)
 
     @patch('guilt.subprocess.Popen')
     def test_run_git_no_output(self, mock_process):
         mock_process.return_value.communicate = Mock(return_value=('', None))
 
-        runner = guilt.GitRunner()
         with self.assertRaises(ValueError):
-            runner._run_git(['log'])
+            self.runner._run_git(['log'])
 
     @patch('guilt.subprocess.Popen')
     def test_run_git_exception(self, mock_process):
         mock_process.return_value.communicate = Mock(side_effect=OSError)
 
-        runner = guilt.GitRunner()
         with self.assertRaises(guilt.GitError):
-            runner._run_git(['log'])
+            self.runner._run_git(['log'])
 
     @patch('guilt.subprocess.Popen')
     def test_run_git(self, mock_process):
         mock_process.return_value.communicate = Mock(return_value=('a\nb\nc', None))
 
-        runner = guilt.GitRunner()
-        self.assertEquals(['a', 'b', 'c'], runner._run_git(['log']))
+        self.assertEquals(['a', 'b', 'c'], self.runner._run_git(['log']))
 
     @patch('guilt.GitRunner._run_git')
     def test_get_delta_files(self, mock_run_git):
         mock_run_git.return_value = ['foo.c', 'foo.h']
 
-        runner = guilt.GitRunner()
         self.assertEquals(
             set(['foo.c', 'foo.h']),
-            runner.get_delta_files('HEAD~1', 'HEAD')
+            self.runner.get_delta_files('HEAD~1', 'HEAD')
         )
 
     @patch('guilt.GitRunner._run_git')
     def test_get_delta_no_files(self, mock_run_git):
         mock_run_git.return_value = []
 
-        runner = guilt.GitRunner()
         with self.assertRaises(ValueError):
-            runner.get_delta_files('HEAD~1', 'HEAD')
+            self.runner.get_delta_files('HEAD~1', 'HEAD')
 
     @patch('guilt.GitRunner._run_git')
     def test_blame_locs(self, mock_run_git):
@@ -109,8 +120,7 @@ class GitRunnerTestCase(TestCase):
         blame.repo_path = 'src/foo.c'
         blame.bucket = {'Foo Bar': 0, 'Tim Pettersen': 0}
 
-        runner = guilt.GitRunner()
-        runner.blame_locs(blame)
+        self.runner.blame_locs(blame)
         self.assertEquals(
                 {'Foo Bar': 2, 'Tim Pettersen': 3},
                 blame.bucket
