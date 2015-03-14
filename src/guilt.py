30a878dd79ba172854f7d02cb23a9735907e97d4
Matt Boyer <mboyer@sdf.org>
2015-03-14 15:20:07 +0000

    Fixed source encoding

--- src/guilt.py
+++ src/guilt.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 # Copyright (c) 2015, Matt Boyer
 # All rights reserved.
 #
@@ -27,7 +28,6 @@
 #     NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 #     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# -*- coding: UTF-8 -*-
 """
 Port of git-guilt to Python
 """
--- test/test_integration.py
+++ test/test_integration.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 # Copyright (c) 2015, Matt Boyer
 # All rights reserved.
 # 
@@ -26,7 +27,6 @@
 #     LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 #     NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 #     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# -*- coding: UTF-8 -*-
 
 from unittest import TestCase
 import subprocess
